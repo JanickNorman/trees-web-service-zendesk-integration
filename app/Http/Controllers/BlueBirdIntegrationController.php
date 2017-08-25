06b5cfe8b04a6d8ede783d815649a68c7ab1b304
Janick Norman <yanicknorman@gmail.com>
2017-08-25 16:03:12 +0700

    tes ganti manifest

--- app/Http/Controllers/BlueBirdIntegrationController.php
+++ app/Http/Controllers/BlueBirdIntegrationController.php
@@ -9,28 +9,29 @@ class BlueBirdIntegrationController extends Controller
     //
     public function admin(Request $request) {
       echo "TES HALLO";
-      // $data['name'] = $request->get('name');
-      // $data['instance_push_id'] = $request->get('instance_push_id');
-      // $data['zendesk_access_token'] = $request->get('zendesk_access_token');
-      //
-      // $data['metadata'] = $metadata = $request->get('metadata');
-      // if (! $metadata) {
-      //   $metadata = json_decode($metadata);
-      //   $data['line_channel_name'] = $metadata['line_channel_name'];
-      //   $data['line_channel_id'] = $metadata['line_channel_id'];
-      //   $data['line_channel_secret'] = $metadata['line_channel_secret'];
-      //   $data['line_channel_access_token'] = $metadata['line_channel_access_token'];
-      //   $data['type'] = "update";
-      // } else {
-      //   $data['type'] = "new";
-      // }
-      // $data['state'] = $request->get('state');
-      // $data['return_url'] = $request->get('return_url');
-      // $data['subdomain'] = $request->get('subdomain');
-      // $data['locale'] = $request->get('subdomain');
-      // $data['error'] = "";
+      dd("HAhAhAhA");
+      $data['name'] = $request->get('name');
+      $data['instance_push_id'] = $request->get('instance_push_id');
+      $data['zendesk_access_token'] = $request->get('zendesk_access_token');
 
-      // return view('bluebirdintegration.admin', $data);
+      $data['metadata'] = $metadata = $request->get('metadata');
+      if (! $metadata) {
+        $metadata = json_decode($metadata);
+        $data['line_channel_name'] = $metadata['line_channel_name'];
+        $data['line_channel_id'] = $metadata['line_channel_id'];
+        $data['line_channel_secret'] = $metadata['line_channel_secret'];
+        $data['line_channel_access_token'] = $metadata['line_channel_access_token'];
+        $data['type'] = "update";
+      } else {
+        $data['type'] = "new";
+      }
+      $data['state'] = $request->get('state');
+      $data['return_url'] = $request->get('return_url');
+      $data['subdomain'] = $request->get('subdomain');
+      $data['locale'] = $request->get('subdomain');
+      $data['error'] = "";
+
+      return view('bluebirdintegration.admin', $data);
       // @name = integration_params[:name]
       // @instance_push_id = integration_params[:instance_push_id]
       // @zendesk_access_token = integration_params[:zendesk_access_token]
@@ -69,7 +70,7 @@ class BlueBirdIntegrationController extends Controller
     }
 
     public function pull() {
-      
+
     }
 
 
